418738a20391aa19539a2c32b31b2f0d23b23fd4
Azein Pwr <azeinpwr@gmail.com>
2016-12-26 19:55:30 +0700

    Save overwrite UI progress, tests still needed

--- app/components/save_load/saveLoad.js
+++ app/components/save_load/saveLoad.js
@@ -2,21 +2,52 @@ import React from 'react';
 import styles from './saveLoad.css';
 import { saveGame, getSavedGames } from '../../engine/game_data/dataWorkers.js';
 
+class ConfirmOverwrite extends React.Component {
+  render(){
+    return(
+      <div className='app__dark__overlay'>
+        <div className={styles.overlay__box}>
+          <div className={styles.overlay__box__text}>
+            Перезаписать выбранный файл?
+          </div>
+          <div className={styles.overlay__box__buttons}>
+            <div className='app__btn__blue' onClick={() => this.props.hide()}>Нет</div>
+            <div className='app__btn__teal' 
+              onClick={() => {this.props.handleSave(this.propskey); this.props.hide() }}>
+              Да
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 export default class SaveLoad extends React.Component {
   constructor(){
     super()
     this.state = {
       saveName: '',
-      savedGames: {}
+      savedGames: {},
+      confirmOverwrite: false,
+      overwriteKey: ''
     }
   }
   
   componentWillMount(){
     this.setState({savedGames: getSavedGames()})
   }
+  
+  hideConfirm = () => {
+    this.setState({confirmOverwrite: false, overwriteKey: ''})
+  }
 
   handleSave = (saveName) => {
     saveGame(saveName, this.props.gameCharacter, this.props.world)
+  }
+
+  handleOverwrite = (saveName) => {
+    this.setState({confirmOverwrite: true, overwriteKey: saveName})
   }
 
   handleSaveName = (e) => {
@@ -30,7 +61,7 @@ export default class SaveLoad extends React.Component {
       <div key={key} className={styles.new__save}>
         <div className={styles.game__name}>{key}</div>
         {this.props.status === 'save' &&
-          <div className='app__btn__teal' onClick={() => this.handleSave(key)}>Сохранить игру</div> 
+          <div className='app__btn__teal' onClick={() => this.handleOverwrite(key)}>Сохранить игру</div> 
         }
         <div className='app__btn__blue'
           onClick={() => this.props.loadGame(this.state.savedGames[key].gameCharacter, this.state.savedGames[key].world)}>
@@ -40,6 +71,9 @@ export default class SaveLoad extends React.Component {
     )
     return(
       <div className={styles.container}>
+        {this.state.confirmOverwrite &&
+          <ConfirmOverwrite hide={this.hideConfirm} handleSave={this.handleSave} key={this.state.overwriteKey} />
+        }
         <div className={styles.box}>
         { this.props.status === 'save' &&
           <div className={styles.new__save}>
